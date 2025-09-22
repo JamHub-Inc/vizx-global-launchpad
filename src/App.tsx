47568b3428a87f42ca81e4b36b9253c89abb258f
Francis3560 <kushkiddeny11@.com>
2025-09-22 12:57:33 -0700

    finalized all the website pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,21 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import RecruitmentProcess from "./pages/RecruitmentProcess";
 import RealEstateSolutions from "./pages/RealEstateSolutions";
-import { ChatProvider } from "@/components/chatContext";
+import BackOffice from "./pages/BackOffice";
+import CX from './pages/CXSolutions';
+import MerchantServices from "./pages/MerchantSolutions";
+import WhyKenya from "./pages/WhyKenya";
+import VizxHI from "./pages/VizxHI";
+import Technology from "./pages/Technology";
+import Healthcare from "./pages/Healthcare";
+import RealEstate from "./pages/RealEstate";
+import Finance from "./pages/Finance";
+import Travel from "./pages/Travel";
+import Logistics from "./pages/Logistics";
+import Retail from "./pages/Retail"
+import Manufuctering from "./pages/Manufuctering";
+import WelfareFund from "./pages/WelfareFund";
+import { ChatProvider } from "@/components/ChatContext";
 import ChatButton from "@/components/chatButton";
 import ChatbotWindow from "@/components/chatWindowComponent";
 
@@ -16,7 +30,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="light" storageKey="vizx-ui-theme">
+    <ThemeProvider defaultTheme="dark" storageKey="vizx-ui-theme">
       <TooltipProvider>
         <Toaster />
         <Sonner />
@@ -26,7 +40,20 @@ const App = () => (
               <Route path="/" element={<Index />} />
               <Route path="rp" element={<RecruitmentProcess />} />
               <Route path="real-estate" element={<RealEstateSolutions />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="back-office" element={<BackOffice />} />
+              <Route path="cx" element={<CX />} />
+              <Route path="merchant" element={< MerchantServices/>} />
+              <Route path="why-kenya" element={<WhyKenya />} />
+              <Route path="vizx-hi-ai" element={<VizxHI />} />
+              <Route path="healthcare" element={<Healthcare />} />
+              <Route path="real-estate-industry" element={<RealEstate />} />
+              <Route path="technology" element={<Technology />} />
+              <Route path="finance" element={<Finance />} />
+              <Route path="travel" element={<Travel />} />
+              <Route path="logistics" element={<Logistics />} />
+              <Route path="manufacturing" element={<Manufuctering />} />
+              <Route path="retail" element={<Retail />} />
+              <Route path="employee-welfare" element={<WelfareFund />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
             {/* Chat components should be outside <Routes>, but inside <ChatProvider> */}
